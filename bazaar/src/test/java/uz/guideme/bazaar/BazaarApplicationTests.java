2d9415b3112db5020e2432a6b2b9004e1bb66254
Rakhmatullo1 <omonovrahmatullo9@gmail.com>
2024-07-12 17:22:31 +0500

    feat: adding new microservice. Bazaar

---
+++ bazaar/src/test/java/uz/guideme/bazaar/BazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.guideme.bazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazaarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
